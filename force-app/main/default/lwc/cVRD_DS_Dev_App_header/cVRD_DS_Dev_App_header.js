fe28cee1801e21b67c26e963d7aef7165e0741d1
Yogesh Digambar <yogesh.digambar@thoughtstorm.ca>
2023-11-30 19:04:05 +0000

    Deploying lightning component with static resources

---
+++ force-app/main/default/lwc/cVRD_DS_Dev_App_header/cVRD_DS_Dev_App_header.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import IMAGES from "@salesforce/resourceUrl/CVRD_Logo";
+
+export default class CVRD_DS_Dev_App_header extends LightningElement {
+    company_logo=IMAGES;
+}
