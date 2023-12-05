82cfd339a23c5672b2c4035ed12800edb667abe4
Yogesh Digambar <yogesh.digambar@thoughtstorm.ca>
2023-12-05 08:07:19 +0000

    Recommitting

--- force-app/main/default/lwc/cVRD_DS_DPAShowScreen/cVRD_DS_DPAShowScreen.js
+++ force-app/main/default/lwc/cVRD_DS_DPAShowScreen/cVRD_DS_DPAShowScreen.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 export default class CVRD_DS_DPAShowScreen extends LightningElement {
 
     showComment1=false;
@@ -8,45 +8,64 @@ export default class CVRD_DS_DPAShowScreen extends LightningElement {
     showError2=false;
     showError3=false;
     showReq=true;
+    @api dpaComment1='';
+    @api dpaComment2='';
+    @api dpaComment3='';
+    @api exemptDPA1=false;
+    @api exemptDPA2=false;
+    @api exemptDPA3=false;
 
 
     handleChange(event){
         console.log('Input1=>'+event.target.name);
         console.log('Input1=>'+event.target.checked);
         if(event.target.name==='input1'){
-            console.log('Input1=>'+event.target.checked);
+
+            this.exemptDPA1=event.target.checked;
             this.showComment1=event.target.checked;
             this.showError1=false;
         }
         if(event.target.name==='input2'){
-            console.log('Input2=>'+event.target.checked);
+
+            this.exemptDPA2=event.target.checked;
             this.showComment2=event.target.checked;
             this.showError2=false;
         }
         if(event.target.name==='input3'){
-            console.log('Input3=>'+event.target.checked);
+
+            this.exemptDPA3=event.target.checked;
             this.showComment3=event.target.checked;
             this.showError3=false;
         }
         //console.log('Input2=>'+event.target.value);
     }
 
+    handlecommentchange(event){
+
+        if(event.target.name==='DPA1'){
+            this.dpaComment1=event.target.value;
+        }
+        if(event.target.name==='DPA2'){
+            this.dpaComment2=event.target.value;
+        }
+        if(event.target.name==='DPA3'){
+            this.dpaComment3=event.target.value;
+        }
+    }
+
     handleBlur(event){
-        console.log('inside id1=>'+event.target.name);
-        console.log('this.showComment1=>'+this.showComment1);
-        console.log('this.showComment2=>'+this.showComment2);
-        console.log('this.showComment3=>'+this.showComment3);
         if(event.target.name==='DPA1' && this.showComment1==true){
-            console.log('inside id1=>'+event.target.value);
+
             if(event.target.value==='' || event.target.value===undefined){
                 this.showError1=true;
+
             }
             else{
                 this.showError1=false;
             }
         }
         if(event.target.name==='DPA2' && this.showComment2==true){
-            console.log('inside id2=>'+event.target.value);
+
             if(event.target.value==='' || event.target.value===undefined){
                 this.showError2=true;
             }
@@ -56,7 +75,7 @@ export default class CVRD_DS_DPAShowScreen extends LightningElement {
             
         }
         if(event.target.name==='DPA3' && this.showComment3==true){
-            console.log('inside id3=>'+event.target.value);
+
             if(event.target.value==='' || event.target.value===undefined){
                 this.showError3=true;
             }
@@ -64,6 +83,23 @@ export default class CVRD_DS_DPAShowScreen extends LightningElement {
                 this.showError3=false;
             }
             
+        }
+    }
+
+    @api validate() {
+
+        // If the component is invalid, return the isValid parameter 
+        // as false and return an error message. 
+        if (((this.dpaComment1 === '' || this.dpaComment1 === undefined) && this.exemptDPA1 === true) ||
+            ((this.dpaComment2 === '' || this.dpaComment2 === undefined) && this.exemptDPA2 === true) ||
+            ((this.dpaComment3 === '' || this.dpaComment3 === undefined) && this.exemptDPA3 === true)) {
+            return {
+                isValid: false,
+                errorMessage: 'Please add comment'
+            };
+        }
+        else {
+            return { isValid: true };
         }
     }
     /*handleBlur1(event){
