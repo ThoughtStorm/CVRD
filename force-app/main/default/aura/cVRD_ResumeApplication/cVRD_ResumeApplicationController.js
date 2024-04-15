1ad4fec75e2615ca0948fdd1180815d899b60796
Yogesh Digambar <yogesh.digambar@thoughtstorm.ca>
2024-04-15 15:20:51 +0000

    Pause & Resume Bugs

---
+++ force-app/main/default/aura/cVRD_ResumeApplication/cVRD_ResumeApplicationController.js
@@ -0,0 +1,24 @@
+({
+    init : function(component, event, helper) {
+        var userId = $A.get("$SObjectType.CurrentUser.Id");
+        helper.populateTable(component, event, helper, userId);
+        
+     },
+      
+     handleMenuSelect: function(component, event, helper) {
+        // Figure out which action was selected
+        var interviewAction = event.getParam("value").split(".");
+        if(interviewAction.includes("resume")) {
+           helper.handleShowModal(component, interviewAction[0]);
+        } else if(interviewAction.includes("delete")) {
+           helper.handleDelete(component, event, helper, interviewAction[0]);
+        }
+     },
+     
+     statusChange: function(component, event) {
+        // When the interview finishes, close the overlay
+        if(event.getParam("status").includes("FINISHED")) {
+           component.get("v.overlay").close();
+        }
+     }
+})
